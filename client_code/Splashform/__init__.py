f411bcca7d42942f74e59dc38b1df113c025e2bc
bismarkcodjoe19@gmail.com <bismarkcodjoe19@gmail.com>
2025-08-12 22:08:22 +0000

    Created nb.png and Splashform; Edited settings; Deleted Form1

--- client_code/Splashform/__init__.py
+++ client_code/Splashform/__init__.py
@@ -1,7 +1,7 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import SplashformTemplate
 from anvil import *
 
-class Form1(Form1Template):
+class Splashform(SplashformTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
