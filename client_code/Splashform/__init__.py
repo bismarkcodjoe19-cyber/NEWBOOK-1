6ff97680f23d4bb4f85c3c925e196ff8ab16325b
bismarkcodjoe19@gmail.com <bismarkcodjoe19@gmail.com>
2025-08-12 23:35:50 +0000

    Edited settings; Deleted lable and Splashform

--- client_code/Splashform/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._anvil_designer import SplashformTemplate
-from anvil import *
-
-class Splashform(SplashformTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
