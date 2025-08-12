4df60e8918e452d8dfc1c591d87368bf8bbf26c1
emmanueladangabe@gmail.com <emmanueladangabe@gmail.com>
2025-08-12 23:06:46 +0000

    Created Form1; Edited settings; Deleted splashform

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,7 +1,8 @@
-from ._anvil_designer import splashformTemplate
+from ._anvil_designer import Form1Template
 from anvil import *
 
-class splashform(splashformTemplate):
+
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
