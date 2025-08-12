13b5ad4bc396e2248c3d793b53088f2ffbb76f6c
emmanueladangabe@gmail.com <emmanueladangabe@gmail.com>
2025-08-12 23:13:45 +0000

    Created Lable; Deleted Form1

--- client_code/Lable/__init__.py
+++ client_code/Lable/__init__.py
@@ -1,8 +1,8 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import LableTemplate
 from anvil import *
 
 
-class Form1(Form1Template):
+class Lable(LableTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
