1e2242eb798d95453af1bae481af6e849ee68ab5
bismarkcodjoe19@gmail.com <bismarkcodjoe19@gmail.com>
2025-08-13 09:45:49 +0000

    Edited Loginform

--- client_code/Loginform/__init__.py
+++ client_code/Loginform/__init__.py
@@ -8,3 +8,7 @@ class Loginform(LoginformTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def youtube_video_1_state_change(self, state, **event_args):
+    """This method is called when the video changes state (eg PAUSED to PLAYING)"""
+    pass
