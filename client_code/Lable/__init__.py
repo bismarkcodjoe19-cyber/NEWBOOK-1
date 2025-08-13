9d55f2afdbc12b2be7b0a30bd2a02b6d3dada41c
bismarkcodjoe19@gmail.com <bismarkcodjoe19@gmail.com>
2025-08-13 00:21:36 +0000

    Edited settings; Deleted Lable

--- client_code/Lable/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._anvil_designer import LableTemplate
-from anvil import *
-
-class Lable(LableTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
