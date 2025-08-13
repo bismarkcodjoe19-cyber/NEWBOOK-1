15d15164b75129a1b535f087843d728eeb341cc0
emmanueladangabe@gmail.com <emmanueladangabe@gmail.com>
2025-08-13 20:53:39 +0000

    Edited forgotpasswordform

--- client_code/forgotpasswordform/__init__.py
+++ client_code/forgotpasswordform/__init__.py
@@ -8,3 +8,11 @@ class forgotpasswordform(forgotpasswordformTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def timer_1_tick(self, **event_args):
+    """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+    pass
+
+  def timer_1_hide(self, **event_args):
+    """This method is called when this timer's form is hidden from the screen (or it is removed from a visible form)"""
+    pass
