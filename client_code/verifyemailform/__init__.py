08260390d4caadac6f2422ec2313fe821591c3d1
emmanueladangabe@gmail.com <emmanueladangabe@gmail.com>
2025-08-15 20:29:25 +0000

    Created verifyemailform

---
+++ client_code/verifyemailform/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import verifyemailformTemplate
+from anvil import *
+
+class verifyemailform(verifyemailformTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def timer_1_tick(self, **event_args):
+    """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+    pass
+
+  def timer_1_show(self, **event_args):
+    """This method is called when this timer's form is shown on the screen (or it is added to a visible form)"""
+    pass
