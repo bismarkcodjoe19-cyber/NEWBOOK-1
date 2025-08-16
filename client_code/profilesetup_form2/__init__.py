223a1d7aa03721b990dc37151bde026990b4488e
bismarkcodjoe19@gmail.com <bismarkcodjoe19@gmail.com>
2025-08-16 23:13:27 +0000

    Edited profilesetup_form2 and suggestionform

--- client_code/profilesetup_form2/__init__.py
+++ client_code/profilesetup_form2/__init__.py
@@ -7,3 +7,7 @@ class profilesetup_form2(profilesetup_form2Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
