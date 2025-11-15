98f31d307e564239e7130ca7d37448f05e72ae5a
bismarkcodjoe19@gmail.com <bismarkcodjoe19@gmail.com>
2025-11-15 11:30:31 +0000

    Edited games

--- client_code/games/__init__.py
+++ client_code/games/__init__.py
@@ -4,16 +4,26 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class gamesgamesTemplate):
+
+class Games(GamesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-  self.game_area.content = """
-<iframe src="/_/theme/ludo/index.html"
-        width="100%"
-        height="600"
-        style="border:none;">
-</iframe>
-"""
+  from ._anvil_designer import GamesTemplate
+from anvil import *
+
+class games(gamesTemplate):
+  def __init__(self, **properties):
+    # Initialise the form and its components
+    self.init_components(**properties)
+
+    # Load the Ludo game into the RichText component
+    self.game_area.content = """
+        <iframe src="/_/theme/ludo/index.html"
+                width="100%"
+                height="600"
+                style="border:none;">
+        </iframe>
+        """
 
