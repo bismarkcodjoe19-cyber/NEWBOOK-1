8e186593c17d9a3acccf5932e5e4590e44adad53
bismarkcodjoe19@gmail.com <bismarkcodjoe19@gmail.com>
2026-04-27 11:26:38 +0000

    Created add_products_form, RP_productcard and dashstore.ItemTemplate11; Edited dashstore; Deleted product_card and product_card.RowTemplate1

--- client_code/RP_productcard/__init__.py
+++ client_code/RP_productcard/__init__.py
@@ -1,10 +1,10 @@
-from ._anvil_designer import RowTemplate1Template
+from ._anvil_designer import RP_productcardTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class RowTemplate1(RowTemplate1Template):
+class RP_productcard(RP_productcardTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
--- client_code/add_products_form/__init__.py
+++ client_code/add_products_form/__init__.py
@@ -1,10 +1,10 @@
-from ._anvil_designer import product_cardTemplate
+from ._anvil_designer import add_products_formTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class product_card(product_cardTemplate):
+class add_products_form(add_products_formTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
